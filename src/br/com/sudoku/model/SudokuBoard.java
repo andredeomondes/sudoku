7724d60265639735fc0aa4f6b7bbf557056528c2
André Deomondes <andredeomondes.dev@gmail.com>
2026-01-04 14:54:44 -0300

    implementa modelo do tabuleiro sudoku

--- src/br/com/sudoku/model/SudokuBoard.java
+++ src/br/com/sudoku/model/SudokuBoard.java
@@ -1,4 +1,45 @@
 package br.com.sudoku.model;
 
 public class SudokuBoard {
+
+    private int[][] board;
+    private boolean[][] fixed;
+
+    public SudokuBoard() {
+        board = new int[9][9];
+        fixed = new boolean[9][9];
+    }
+
+    public void setFixed(int row, int col, int value) {
+        board[row][col] = value;
+        fixed[row][col] = true;
+    }
+
+    public boolean isFixed(int row, int col) {
+        return fixed[row][col];
+    }
+
+    public int get(int row, int col) {
+        return board[row][col];
+    }
+
+    public void set(int row, int col, int value) {
+        if (!fixed[row][col]) {
+            board[row][col] = value;
+        }
+    }
+
+    public boolean isEmpty(int row, int col) {
+        return board[row][col] == 0;
+    }
+
+    public void clearUserNumbers() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (!fixed[i][j]) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
 }
